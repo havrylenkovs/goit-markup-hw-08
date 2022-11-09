e6160679f29f6be3401dcc278fea27b0f3c72e57
Vladyslav <110973854+havrylenkovs@users.noreply.github.com>
2022-11-10 01:04:45 +0200

    update to mobile menu

--- js/menu.js
+++ js/menu.js
@@ -1,17 +1,14 @@
 (() => {
   const refs = {
-    openMenuBtn: document.querySelector("[data-menu-open]"),
-    closeMenuBtn: document.querySelector("[data-menu-close]"),
-    menu: document.querySelector("[data-menu]"),
-    body: document.querySelector("body"),
+    openMenuBtn: document.querySelector('[data-menu-open]'),
+    closeMenuBtn: document.querySelector('[data-menu-close]'),
+    menu: document.querySelector('[data-menu]'),
   };
 
-  refs.openMenuBtn.addEventListener("click", toggleMenu);
-  refs.closeMenuBtn.addEventListener("click", toggleMenu);
+  refs.openMenuBtn.addEventListener('click', toggleMenu);
+  refs.closeMenuBtn.addEventListener('click', toggleMenu);
 
   function toggleMenu() {
-    refs.menu.classList.toggle("is-hidden");
-    refs.body.classList.toggle("no-scroll");
+    refs.menu.classList.toggle('is-hidden');
   }
-
 })();
